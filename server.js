db3290b6784a876025cb3a2f621f40c87c0f09d6
rabell66 <raabell66@gmail.com>
2017-07-11 13:21:28 -0500

    updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require("express-session");
 const sessionConfig = require("./sessionConfig");
 const port = process.env.PORT || 8000;
 const app = express();
-var users = [{userName: "a", password:"a"}];
+var users = [{ userName: "a", password: "a" }];
 
 //Set View Engine
 app.engine("mustache", mustacheExpress());
@@ -17,14 +17,14 @@ app.use("/", express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
 //routes
-  
-app.get("/",function(req, res) {
-    console.log(req.session.user);
-    // if(req.session.user){
-  res.render("index",{user:req.session.user});
-    // } else {
-        //   res.render("index");
-    // }
+
+app.get("/", function(req, res) {
+  console.log(req.session.user);
+  // if(req.session.user){
+  res.render("index", { user: req.session.user });
+  // } else {
+  //   res.render("index");
+  // }
 });
 
 app.get("/signup", function(req, res) {
@@ -35,8 +35,7 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/login", function(req, res) {
-   
-  if (!req.body || !req.body.userName || !req.body.password){
+  if (!req.body || !req.body.userName || !req.body.password) {
     console.log("No data sent");
     return res.redirect("/login");
   }
@@ -61,14 +60,14 @@ app.post("/login", function(req, res) {
     req.session.user = userRecord;
     return res.redirect("/");
   } else {
-      console.log("Wrong password");
+    console.log("Wrong password");
     return res.redirect("/login");
   }
 });
 
-app.post("/users",function(req, res) {
-    console.log('userName in Body = ');
-    console.log(req.body.password);
+app.post("/users", function(req, res) {
+  console.log("userName in Body = ");
+  console.log(req.body.password);
   if (!req.body || !req.body.userName || !req.body.password) {
     return res.redirect("/");
   }
@@ -79,19 +78,15 @@ app.post("/users",function(req, res) {
   };
   //add user to user array
   users.push(newUser);
- 
+
   return res.redirect("/");
 });
 
 //LOGOUT
-app.post('/logout', function (req, res) {
-    req.session.destroy();
-    res.redirect('/');
+app.post("/logout", function(req, res) {
+  req.session.destroy();
+  res.redirect("/");
 });
-
-
-
-
 
 app.listen(port, function() {
   console.log("Server is running on port", port);
